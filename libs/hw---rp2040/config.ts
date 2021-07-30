802678ba88b9321dca04ae97efee1b7b4e383860
riven <4771215@qq.com>
2021-07-30 16:52:19 +0800

    add board config~

--- libs/hw---rp2040/config.ts
+++ libs/hw---rp2040/config.ts
@@ -1,3 +1,18 @@
 namespace config {
- export const PIN_P0 = 0 
+ // screen
+ export const PIN_DISPLAY_SCK = DAL.P22
+ export const PIN_DISPLAY_MOSI = DAL.P23
+ export const PIN_DISPLAY_MISO = DAL.P20
+ export const PIN_DISPLAY_DC = DAL.P21
+ export const PIN_DISPLAY_RST = DAL.P25
+ export const PIN_DISPLAY_CS = DAL.P24
+
+ export const DISPLAY_WIDTH = 160
+ export const DISPLAY_HEIGHT = 128
+
+ export const DISPLAY_TYPE = 7735
+ export const DISPLAY_CFG0 = 0x00000080
+ export const DISPLAY_CFG1 = 0x00000603
+ export const DISPLAY_CFG2 = 8
+
 }
